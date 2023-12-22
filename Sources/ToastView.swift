2665141d8fd98ebcd5c5426a58d284cc5c25d500
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-12-22 19:09:50 +0600

    updated

---
+++ Sources/ToastView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Joynal Abedin on 22/12/23.
+//
+
+import SwiftUI
+
+public struct ToastView: View {
+    public var body: some View {
+        Text("Hello, World! Toast view")
+    }
+}
+
+//#Preview {
+//    ToastView()
+//}
