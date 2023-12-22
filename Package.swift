378cc0f56ff3aa33b1e6fb571a8ccc61a554d040
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-12-22 19:22:04 +0600

    sources added

--- Package.swift
+++ Package.swift
@@ -15,7 +15,9 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ToastAlertSwiftPackage"),
+            name: "ToastAlertSwiftPackage",
+            path: "Sources"
+        ),
         .testTarget(
             name: "ToastAlertSwiftPackageTests",
             dependencies: ["ToastAlertSwiftPackage"]),
