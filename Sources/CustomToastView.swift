bf4b627804682a442805a6c82c8ee5e0d6470437
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-12-24 00:08:59 +0600

    overlay rectangle width changed

--- Sources/CustomToastView.swift
+++ Sources/CustomToastView.swift
@@ -55,7 +55,7 @@ public struct CustomToastView: View {
         .overlay(
             Rectangle()
                 .fill(toastType.themeColor)
-                .frame(width: 6)
+                .frame(width: toastTitle.isEmpty || toastMessage.isEmpty ? 2 : 6)
                 .clipped()
             , alignment: .leading
         )
